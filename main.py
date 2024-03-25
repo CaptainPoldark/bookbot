cf4b6231a7c12d564dada5d3419d3a026ada5319
captainpoldark <jonathankylebrooks@gmail.com>
2024-03-24 23:50:44 -0400

    added analysis functions to print the number of words, and occurences of an alphabetical charecter sorted by most-common to least

---
+++ main.py
@@ -0,0 +1,48 @@
+from collections import OrderedDict
+def main():
+    base_path = "books/"
+    file_name = "frankenstein.txt"
+    file_path = base_path + file_name
+    text = read_file(file_path)
+    num_words = count_words(text)
+    character_dict = count_characters(text)
+    print(f"-- Begin report of {file_name} --")
+    print(f"The book has {num_words} words.")
+    print("")
+
+    for character, count in character_dict.items():
+        print(f"The '{character}' was found {count} times.")
+    print("-- End of report --")
+
+def sort_dict_by_value(dictionary):
+    sorted_dict = OrderedDict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))
+    return sorted_dict
+
+def count_characters(text):
+    character_dict = {}
+    for character in text:
+        character_lowered = character.lower()
+        if character_lowered.isalpha():
+            if character_lowered in character_dict:
+                character_dict[character_lowered] += 1
+            else:
+                character_dict[character_lowered] = 1
+
+    sorted_dict = sort_dict_by_value(character_dict)
+
+    return sorted_dict
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def read_file(file_path):
+    try:
+        with open(file_path) as file:
+            text = file.read()
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        text = ""
+    return text
+
+main()
